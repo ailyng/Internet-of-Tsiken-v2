551a0b832eabb41168b1e8f4f8997b4f1328742c
Doubler R. Gammad <gammad.doubler.riel@gmail.com>
2025-11-09 19:04:28 +0800

    firebase configuration

--- screens/firebase.js
+++ screens/firebase.js
@@ -1,25 +1,25 @@
-// firebase.js
-// Simulated Firebase connection — ready for your real credentials later
-
 // If you haven’t yet installed Firebase, run:
 // 👉 npm install firebase
 
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
-// ⚠️ Replace these with your own Firebase config later
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyA_UhQ_KlCsoCmRlda_S2RrUv6QhGo0c_0",
+  authDomain: "internet-of-tsiken.firebaseapp.com",
+  projectId: "internet-of-tsiken",
+  storageBucket: "internet-of-tsiken.firebasestorage.app",
+  messagingSenderId: "998028163151",
+  appId: "1:998028163151:web:ce0864acced24f37d7d23a",
+  measurementId: "G-W7495LQB4J"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Analytics
+const analytics = getAnalytics(app);
 
 // Initialize Firebase Authentication and Firestore
 export const auth = getAuth(app);
