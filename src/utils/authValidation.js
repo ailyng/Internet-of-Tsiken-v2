749db3c3a31d6f1a11e707cfca35d88f7468965d
Ailyn <gilvero.ailynmay.franceliso@gmail.com>
2025-11-21 22:37:49 +0800

    Add auth validation functions for email and password

---
+++ src/utils/authValidation.js
@@ -0,0 +1,12 @@
+function validateEmail(email) {
+    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validatePassword(password) {
+    // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return re.test(password);
+}
+
+module.exports = { validateEmail, validatePassword };
